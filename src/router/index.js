7acc92c64b3d7242ff0ea4943b722502e3729b89
张冉 <1607689880@qq.com>
2018-06-04 17:09:02 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/static/webstatic/register/login.html'
+      redirect: '/loginIndex'
     },
     work,
     market,
