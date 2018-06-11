86cd29623c95309cf18c5f54167efedd01d24d53
panzhenfei <604501547@qq.com>
2018-06-11 13:59:57 +0800

    劳务

--- src/components/laowu/sign.vue
+++ src/components/laowu/sign.vue
@@ -3,7 +3,7 @@
 
        <header class="mui-bar mui-bar-nav">
 
-	<h1 @click="cc" class="mui-title">上工打卡</h1>
+	<h1  class="mui-title">上工打卡</h1>
 	<button id="btn-referrer" class="mui-btn mui-btn-link mui-btn-nav mui-pull-left hide">
 		<span class="mui-icon mui-icon-back"></span>
 	</button>
@@ -48,7 +48,7 @@
 		<a class="item" @click="delayRemind('day')">今日不上工<span class="mui-icon mui-icon-arrowright"></span></a>
 	</p>
 	<div class="sign-btn">
-			<span @click="sign();" class="title">
+			<span @click="project_sign.sign();" class="title">
 				<span class="mui-icon mui-icon-camera"></span>
 				<span class="txt">点击打卡</span>
 			</span>
@@ -109,11 +109,11 @@
     </template>
 
 <script>
-import project_sign from "./project_sign.js"
-import laowu_common from "./laowu_common.js"
+import project_sign from  "./project_sign.js"
 export default {
     data(){
         return {
+            project_sign:project_sign,
             form:{
             id:"",
             userId:"",
@@ -133,6 +133,7 @@ export default {
             img_url:"",//项目图片
             remindTime:"",//提醒期限
             remindName:"",//提醒期限
+            
         },
         reqWorkParamsVO:{
             userId:"",
@@ -157,7 +158,6 @@ export default {
         select_time:"",
         select_time_name:"",
         projectList:[],
-        //imgurl:UPLOAD_SERVER_ADDRESS,
         nearRecord:false,
         nearZuobiao:"",//最近打卡记录的坐标
         nearAddress:"",//最近打卡记录的地址
@@ -165,19 +165,17 @@ export default {
         }
     },
     created:function(){
-     project_sign.axios=this.$http
-     project_sign._self=this
+	project_sign._self=this;
+     project_sign.initVue()
+     project_sign.initData()
+     
        
     },
      methods:{
         goback(){
             this.$router.go(-1);
         },
-        cc(){
-           alert(laowu_common.userId)
-         project_sign.initData()
-           console.log($("header").text())
-        }
+      
     },
     mounted(){
 
