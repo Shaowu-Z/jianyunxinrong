eb187bd05d1cfb80ef134ca756f1c77cdaa8eb85
chenbende <1466462952@qq.com>
2018-06-22 11:44:56 +0800

    组合查询和收藏路径

--- src/router/work.js
+++ src/router/work.js
@@ -9,6 +9,25 @@ const workIndex = resolve => require(['@/components/work/workIndex'], resolve)
 const user = resolve => require(['@/components/work/user'], resolve)
 const frr = resolve => require(['@/components/work/user/frr'], resolve)
 const cli = resolve => require(['@/components/work/user/cli'], resolve)
+const simplify = resolve => require(['@/components/work/simplify'], resolve)
+/**
+ * 组合查询
+ */
+const  chengnuo = resolve => require(['@/components/work/simplify/simplifyChengnuo'], resolve)
+const contract = resolve => require(['@/components/work/simplify/simplifyContract'], resolve)
+const contractMingxi = resolve => require(['@/components/work/simplify/simplifyContractMingxi'], resolve)
+const fapiao = resolve => require(['@/components/work/simplify/simplifyFapiao'], resolve)
+const fapiaoMingxi = resolve => require(['@/components/work/simplify/simplifyFapiaoMingxi'], resolve)
+const gongzuolianxidan = resolve => require(['@/components/work/simplify/simplifyGongzuolianxidan'], resolve)
+const huiyijiyao = resolve => require(['@/components/work/simplify/simplifyHuiyijiyao'], resolve)
+const jindujian = resolve => require(['@/components/work/simplify/simplifyJindujian'], resolve)
+const laowubaoliang = resolve => require(['@/components/work/simplify/simplifyLaowubaoliang'], resolve)
+const laowubaoliangMingxi = resolve => require(['@/components/work/simplify/simplifyLaowubaoliangMingxi'], resolve)
+const paigongdan = resolve => require(['@/components/work/simplify/simplifyPaigongdan'], resolve)
+const paigongdanMingxi = resolve => require(['@/components/work/simplify/simplifyPaigongdanMingxi'], resolve)
+const shoufukuan = resolve => require(['@/components/work/simplify/simplifyShoufukuan'], resolve)
+const shouliao = resolve => require(['@/components/work/simplify/simplifyShouliao'], resolve)
+const shouliaoMingxi = resolve => require(['@/components/work/simplify/simplifyShouliaoMingxi'], resolve)
 
 const workConfig = {
   path: '/work',
@@ -23,6 +42,10 @@ const workConfig = {
       component: user
     },
     {
+      path: '/',
+      component: simplify
+    },
+    {
       path: '/user',
       component: user,
       children: [
@@ -34,6 +57,73 @@ const workConfig = {
           path: 'cli',
           component: cli
         }
+      ]
+    },
+    {
+      path: '/simplify',
+      component: simplify,
+      children: [
+        {
+          path: '/static/webstatic/work/simplify/simplify_chengnuo.html',
+          component: chengnuo
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_contract.html',
+          component: contract
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_contract_mingxi.html',
+          component: contractMingxi
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_fapiao.html',
+          component: fapiao
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_fapiao_mingxi.html',
+          component: fapiaoMingxi
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_gongzuolianxidan.html',
+          component: gongzuolianxidan
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_huiyijiyao.html',
+          component: huiyijiyao
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_jindujian.html',
+          component: jindujian
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_laowubaoliang.html',
+          component: laowubaoliang
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_laowubaoliang_mingxi.html',
+          component: laowubaoliangMingxi
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_paigongdan.html',
+          component: paigongdan
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_paigongdan_mingxi.html',
+          component: paigongdanMingxi
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_shoufukuan.html',
+          component: shoufukuan
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_shouliao.html',
+          component: shouliao
+        },
+        {
+          path: '/static/webstatic/work/simplify/simplify_shouliao_mingxi.html',
+          component: shouliaoMingxi
+        }
+
       ]
     }
   ]
