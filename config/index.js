01d24da1548e3eb41ebfc221058f2aa86035e7ae
张冉 <1607689880@qq.com>
2018-06-26 20:41:15 +0800

    啊

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
   // },
   dev: {
     env: require('./dev.env'),
-    host:"192.168.0.102",
+    host:"192.168.0.99",
     port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
